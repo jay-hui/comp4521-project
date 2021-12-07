de7fd67f0417d7a08d675518457885d9b3eee73a
javier <29498758+javier-hui@users.noreply.github.com>
2021-12-08 00:17:20 +0800

    button

--- app/src/main/java/com/example/wellhydrated/MainActivity.java
+++ app/src/main/java/com/example/wellhydrated/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.wellhydrated;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private int waterCount = 0;
+    private TextView labelWaterAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        labelWaterAmount = findViewById(R.id.label_water_amount);
+        Log.d("MainActivity", "main activity started");
+    }
+
+    public void drinkWater(View view) {
+        waterCount++;
+        String newText = "You drank " + waterCount + " cups of water today!";
+        labelWaterAmount.setText(newText);
+
+        Toast toast = Toast.makeText(this, R.string.toast_drink_water, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }

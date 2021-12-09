162ef91c570c0543943712346c5685223d946531
Eric Leung <waichakleung@gmail.com>
2021-12-09 16:07:23 +0800

    Database Creation Implemented

--- app/src/main/java/com/example/wellhydrated/DBHelper.java
+++ app/src/main/java/com/example/wellhydrated/DBHelper.java
@@ -1,2 +1,32 @@
-package com.example.wellhydrated;public class DBHelper {
+package com.example.wellhydrated;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "DrinkWaterHistory.db";
+    public static final String TABLE_NAME = "History";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_ENTRIES = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                                                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                "drink_date TEXT," +
+                                                "drink_time TEXT," +
+                                                "amount INTEGER)";
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        //db.execSQL(SQL_DELETE_ENTRIES);
+        //onCreate(db);
+    }
 }

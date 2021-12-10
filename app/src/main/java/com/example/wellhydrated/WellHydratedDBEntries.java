92097a8d2aa5aa9addb34392a90087905f147ea5
Eric Leung <waichakleung@gmail.com>
2021-12-11 02:27:22 +0800

    Added the column ID as well

--- app/src/main/java/com/example/wellhydrated/WellHydratedDBEntries.java
+++ app/src/main/java/com/example/wellhydrated/WellHydratedDBEntries.java
@@ -5,6 +5,7 @@ import android.provider.BaseColumns;
 public class WellHydratedDBEntries implements BaseColumns {
     // Just define all constants, such as table names, column names
     public static final String TABLE_NAME = "wellhydrated_records";
+    public static final String COLUMN_NAME_ID = "id";
     public static final String COLUMN_NAME_DRINK_DATE = "drink_date";
     public static final String COLUMN_NAME_DRINK_TIME = "drink_time";
     public static final String COLUMN_NAME_AMOUNT = "amount";

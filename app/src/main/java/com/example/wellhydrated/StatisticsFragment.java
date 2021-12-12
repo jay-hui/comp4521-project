ab8cf68db10dcd6cb5a6809e8f511276c9b43856
Eric Leung <waichakleung@gmail.com>
2021-12-13 05:04:01 +0800

    Before Rebase

--- app/src/main/java/com/example/wellhydrated/StatisticsFragment.java
+++ app/src/main/java/com/example/wellhydrated/StatisticsFragment.java
@@ -217,4 +217,29 @@ public class StatisticsFragment extends Fragment {
         graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
         graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
     }
+
+    public Cursor getHistory(int year, int month) {
+        String startOfMonth;
+        if (month >=1 && month <= 9)
+            startOfMonth = String.format("%d-0%d-01", year, month);
+        else if (month >= 10 & month <= 12)
+            startOfMonth = String.format("%d-%d-01", year, month);
+        else
+            // Invalid month
+            return null;
+
+        //SELECT drink_date, SUM(amount) AS total_amount
+        //FROM wellhydrated_records
+        //WHERE drink_date BETWEEN (SELECT date("now", "-6 day")) AND (SELECT date("now"))
+        //      AND amount <> 0?
+        //GROUP BY drink_date
+        //ORDER BY drink_date
+        String query = "SELECT " + WellHydratedDBEntries.COLUMN_NAME_DRINK_DATE + ", SUM(" + WellHydratedDBEntries.COLUMN_NAME_AMOUNT + ") AS total_amount " +
+                "FROM " + WellHydratedDBEntries.TABLE_NAME + " " +
+                "WHERE " + WellHydratedDBEntries.COLUMN_NAME_DRINK_DATE + " BETWEEN " + startOfMonth + " AND (SELECT date('" + startOfMonth + "', '+1 month', '-1 day')) " +
+                "GROUP BY " + WellHydratedDBEntries.COLUMN_NAME_DRINK_DATE + " " +
+                "ORDER BY " + WellHydratedDBEntries.COLUMN_NAME_DRINK_DATE;
+
+        return dbReadable.rawQuery(query, null);
+    }
 }
